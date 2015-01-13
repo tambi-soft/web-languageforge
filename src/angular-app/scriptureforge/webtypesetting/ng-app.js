7d92fe4220e9058c523abbe5d223853cc4ccd2cd
Chris Hirt <chris_hirt@sil.org>
2015-01-13 09:42:03 +0700

    stub out ui-route for webtypesetting (projectSetup)

--- src/angular-app/scriptureforge/webtypesetting/ng-app.js
+++ src/angular-app/scriptureforge/webtypesetting/ng-app.js
@@ -8,16 +8,27 @@ angular.module('webtypesetting',
 		])
 	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 		
-		$urlRouterProvider.otherwise('/setup');
+		$urlRouterProvider.otherwise('/');
 		
-	    $stateProvider        
-		    .state('setupProject', {
+	    $stateProvider
+	    	.state('home', {
+	    		url: '/',
+	    		templateUrl: '/angular-app/scriptureforge/webtypesetting/views/home.html'
+	    	})
+		    .state('projectSetup', {
 		        url: '/setup',
-		        views: {
-		        	'': {templateUrl: '/angular-app/scriptureforge/webtypesetting/views/projectSetup.html',
-		        		controller: 'projectSetupCtrl'}
-		        }
-		    });
+		        templateUrl: '/angular-app/scriptureforge/webtypesetting/views/projectSetup.html'
+		    })
+	    	.state('projectSetup.assets', {
+	    		url: '/assets',
+	    		templateUrl: '/angular-app/scriptureforge/webtypesetting/views/projectSetup.assets.html'
+	    	})
+	    	.state('projectSetup.layout', {
+	    		url: '/layout',
+	    		templateUrl: '/angular-app/scriptureforge/webtypesetting/views/projectSetup.layout.html'
+	    	})
+	    	
+		    ;
 	    
 	}])
 	.controller('MainCtrl', ['$scope', function($scope) {
