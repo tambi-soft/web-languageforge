8dca2a44820e553099deaf946df76091e37eff7a
JoshWilhelm <wilhelm.josh@gmail.com>
2015-01-14 09:59:57 +0700

    wip #6 - Project Settings Layout page
    - numeric values for margins, functional visual representation of margins, save button
    TODO - breadcrumbs for location, move margin validation into the controller, units?, breadcrumbs

--- src/angular-app/scriptureforge/webtypesetting/views/projectSetup.js
+++ src/angular-app/scriptureforge/webtypesetting/views/projectSetup.js
@@ -1,8 +1,7 @@
 'use strict';
-
 angular.module('webtypesetting.projectSetup', ['jsonRpc', 'ui.bootstrap', 'bellows.services',  'ngAnimate', 'palaso.ui.notice', 'webtypesetting.services'])
 
 .controller('projectSetupCtrl', ['$scope', '$state', 'webtypesettingSetupService',  'sessionService', 'modalService', 'silNoticeService',
 function($scope, $state, webtypesettingSetupApi, sessionService, modal, notice) {
-	
+
 }]);
--- src/angular-app/scriptureforge/webtypesetting/views/projectSetup.layout.js
+++ src/angular-app/scriptureforge/webtypesetting/views/projectSetup.layout.js
@@ -1,1 +1,21 @@
 // controller for setupProjectLayout
+'use strict';
+
+angular.module('webtypesetting.projectSetup', ['jsonRpc', 'ui.bootstrap', 'bellows.services', 'ngAnimate', 'palaso.ui.notice', 'webtypesetting.services'])
+
+.controller('projectSetupCtrl', ['$scope', '$state', 'webtypesettingSetupService', 'sessionService', 'modalService', 'silNoticeService', function($scope, $state, webtypesettingSetupApi, sessionService, modal, notice) {
+
+}])
+.controller('projectSetupLayoutCtrl', ['$scope', '$state', 'webtypesettingSetupService', 'sessionService', 'modalService', 'silNoticeService', function($scope, $state, webtypesettingSetupApi, sessionService, modal, notice) {
+  var vm = this;
+  vm.width = 400;
+  vm.height = 500;
+  vm.insideMargin = 60;
+  vm.outsideMargin = 50;
+  vm.topMargin = 35;
+  vm.bottomMargin = 40;
+  vm.lockHorizontalMargins = false;
+  vm.lockVerticalMargins = false;
+  // vm.units = "pixels";
+  // two way binding?
+}]);
