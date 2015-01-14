b5c5d137a9c876356ac68418efd1dda99e83f52d
Cambell <cambell.prince@gmail.com>
2015-01-14 22:54:38 +0700

    Fix #28: Primary nav pills

--- src/angular-app/scriptureforge/webtypesetting/ng-app.js
+++ src/angular-app/scriptureforge/webtypesetting/ng-app.js
@@ -10,6 +10,15 @@ angular.module('webtypesetting',
 		 'webtypesetting.projectSetupLayout',
 		 'webtypesetting.projectSetupAssets'
 		])
+	.run(['$rootScope', '$state', '$stateParams', function ($rootScope,   $state,   $stateParams) {
+	    // Add $state and $stateParams to the $rootScope so that we can access them from any scope.
+	    // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+	    // to active whenever 'contacts.list' or one of its decendents is active.
+	    $rootScope.$state = $state;
+	    $rootScope.$stateParams = $stateParams;
+	    }
+	  ]
+	)
 	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 		
 		$urlRouterProvider.otherwise('/');
@@ -19,25 +28,38 @@ angular.module('webtypesetting',
 	    		url: '/',
 	    		templateUrl: '/angular-app/scriptureforge/webtypesetting/views/home.html'
 	    	})
-		    .state('projectSetup', {
-		        url: '/setup',
-		        templateUrl: '/angular-app/scriptureforge/webtypesetting/views/projectSetup.html'
-		    })
-	    	.state('projectSetup.assets', {
+//		    .state('projectSetup', {
+//		        url: '/setup',
+//		        templateUrl: '/angular-app/scriptureforge/webtypesetting/views/projectSetup.html'
+//		    })
+	    	.state('projectSetupAssets', {
 	    		url: '/assets',
 	    		templateUrl: '/angular-app/scriptureforge/webtypesetting/views/projectSetup.assets.html'
 	    	})
-	    	.state('projectSetup.layout', {
+	    	.state('projectSetupLayout', {
 	    		url: '/layout',
 	    		templateUrl: '/angular-app/scriptureforge/webtypesetting/views/projectSetup.layout.html'
 	    	})
+//	    	.state('composition', {
+//		        url: '/composition',
+//		        views: {
+//		        	'': {templateUrl: '/angular-app/scriptureforge/webtypesetting/views/composition.html',
+//		        		controller: 'compositionCtrl'}
+//		        }
+//		    })
 	    	.state('composition', {
 		        url: '/composition',
-		        views: {
-		        	'': {templateUrl: '/angular-app/scriptureforge/webtypesetting/views/composition.html',
-		        		controller: 'compositionCtrl'}
-		        }
-		    });
+		        templateUrl: '/angular-app/scriptureforge/webtypesetting/views/composition.html'
+		    })
+	    	.state('discussion', {
+	    		url: '/discussion',
+	    		templateUrl: '/angular-app/scriptureforge/webtypesetting/views/discussionList.html'
+	    	})
+	    	.state('render', {
+	    		url: '/render',
+	    		templateUrl: '/angular-app/scriptureforge/webtypesetting/views/renderList.html'
+	    	})
+		    ;
 	    	
 		    
 	    
