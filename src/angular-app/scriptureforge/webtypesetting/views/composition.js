117714ebfc0c4224f2760ee729baf339300e2229
Daniel <dgd8@students.calvin.edu>
2015-01-15 14:16:08 +0700

    added change growth factor input

--- src/angular-app/scriptureforge/webtypesetting/views/composition.js
+++ src/angular-app/scriptureforge/webtypesetting/views/composition.js
@@ -18,20 +18,15 @@ angular.module(
 						compositionService, sessionService, modal,
 						notice) {
 					
-					$scope.paragraphProperties = {
-							c1v1: {growthfactor:0},
-							c2v1: {growthfactor:1},
-							c3v1: {growthfactor:0},
-							c4v1: {growthfactor:3},
+					var paragraphProperties = {
+							//c1v1: {growthfactor:3},
 							};
 
 					$scope.selectedPage = 1;
 					$scope.bookID = 1;
 					$scope.selectedText="";
 					$scope.properties = "test";
-					$scope.bookHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean imperdiet semper luctus. Fusce feugiat urna orci, ut finibus metus eleifend a. Sed nibh quam, efficitur vitae lacus in, maximus malesuada elit. Nulla vulputate eget tellus nec tristique. Nulla blandit vitae arcu eu mollis. Integer ut mauris ut sem elementum fringilla ac ac diam. In non placerat arcu. Pellentesque eu mollis velit."+
-						"Sed sed vestibulum felis, id posuere purus. Sed eget sodales ex, porta blandit ante. Cras vel velit nec nisl placerat eleifend a vel urna. Praesent in elementum neque, eget gravida enim. Donec eget quam sed eros ullamcorper pellentesque. Praesent tellus turpis, maximus et mi non, tristique condimentum enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce feugiat sit amet felis eu ultrices."+
-						"Mauris tincidunt purus augue, sit amet faucibus nisi aliquet et. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse tempus volutpat dictum. Maecenas ut consectetur lacus. Vivamus pharetra ante vel lorem hendrerit, a consectetur nisl interdum. Phasellus interdum nulla magna. Nulla sem elit, gravida quis lobortis quis, rhoncus ac enim.";
+					$scope.bookHTML = "";
 					$scope.renderRapuma = function() {
 						compositionService.renderBook(function(result) {
 							if (result.ok) {
@@ -73,11 +68,24 @@ angular.module(
 					};
 					$scope.getBookHTML();
 					$scope.$watch('paragraphNode', function(){
+						if(!$scope.paragraphNode)return;
 						$scope.selectedText = $scope.paragraphNode.innerHTML;
 						$scope.verse = $scope.paragraphNode.firstElementChild.innerText;
-						while(!$scope.paragraphNode.innerText || $scope.paragraphNode.innerText.indexOf("Chapter") == -1){
-							$scope.paragraphNode = $scope.paragraphNode.previousSibling;
+						$scope.temp = $scope.paragraphNode;
+						while(!$scope.temp.innerText || $scope.temp.innerText.indexOf("Chapter") == -1){
+							$scope.temp = $scope.temp.previousSibling;
 						}
-						$scope.chapter = $scope.paragraphNode.innerText.replace("Chapter ", "");
+						$scope.chapter = $scope.temp.innerText.replace("Chapter ", "");
+						var currentVerse = 'c'+$scope.chapter+'v'+$scope.verse;
+						var currentParagraphProperties = paragraphProperties[currentVerse];
+						$scope.paragraphGrowthFactor =  (currentParagraphProperties)? currentParagraphProperties.growthfactor : 0;
+					});
+					$scope.$watch('paragraphGrowthFactor', function() {
+						if(!currentVerse)return;
+						if(paragraphProperties[currentVerse]){
+							paragraphProperties[currentVerse].growthfactor = $scope.paragraphGrowthFactor;
+						} else {
+							paragraphProperties[currentVerse] = {growthfactor:$scope.paragraphGrowthFactor};
+						}
 					});
 				} ]);
