f43f5d83fe04269d679de031b3eee409450288a3
mattrowemk <mlr28@students.calvin.edu>
2015-01-15 14:53:37 +0700

    wip #8 e2e update composition page object

--- test/app/scriptureforge/webtypesetting/pages/compositionPage.js
+++ test/app/scriptureforge/webtypesetting/pages/compositionPage.js
@@ -3,16 +3,28 @@
 //location to put all of the backend so that the test page stays clear.
 
 var SfCompositionPage = function() {
-	
+
 	this.navigation = {
 			composition: element(by.cssContainingText('here', 'Composition')),
 	};
-	
+
 	this.slider = {
-	leftButton: element(by.partialButtonText('left')),
-	rightButton: element(by.partialButtonText('right')),
-	goButton: element(by.partialButtonText('GO')),
-	pageInput: element(protractor.By.css('.pageNumber')),
+			leftButton: element(by.partialButtonText('left')),
+			rightButton: element(by.partialButtonText('right')),
+			goButton: element(by.partialButtonText('GO')),
+			pageInput: element(protractor.By.css('.pageNumber')),
+			//slider: element() //need to be able to access slider functionality, not as simple as button presses
+	};
+
+	this.left = {
+			//text	
+			//paragraph settings
+
+	};
+
+	this.right = {
+			renderButton: element(by.partialButtonText('render')),
+			//image:
 	};
 
 };
