83bb9593896257b9a1d85bf36bcabfc3696a4a7e
Adam7700 <adampog77@gmail.com>
2015-01-19 10:04:41 +0800

    wip #6 Added e2e tests. tests can reach layout page, make sure margin input boxes can be filled out. it visually changes css, but still need to confirm it through more tests. Also still need to add more tests in general for the layout page.

---
+++ test/app/scriptureforge/layoutTests/e2e/projectLayoutPage.spec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+describe('the project dashboard AKA text list page', function() {
+	var constants 			= require('../../../testConstants.json');
+	var loginPage 			= require('../../../bellows/pages/loginPage.js');
+	var util 				= require('../../../bellows/pages/util.js');
+	var appFrame 			= require('../../../bellows/pages/appFrame.js');
+	var projectListPage 	= require('../../../bellows/pages/projectsPage.js');
+	var projectLayoutPage   = require('../../../bellows/pages/projectsLayoutPage.js');
+	var projectPage 		= require('../../sfchecks/pages/projectPage.js');
+	var projectLayout 		= require('../pages/projectLayoutPage.js');
+	
+	
+	describe('project member/user', function() {
+		it('setup: logout, login as project member, go to project dashboard', function() {
+			loginPage.logout();
+			loginPage.loginAsMember();
+		});
+	});
+		
+	describe('Layout page', function(){
+		
+		it("setup: login, go to layout page", function(){
+			
+			loginPage.loginAsMember();
+			projectLayoutPage.get();
+		});
+	
+		it('should have text: Inside Margin', function(){
+			expect(projectLayout.innerMarginForm.isDisplayed()).toBe(true);
+		});
+		it('should have text: Outside Margin', function(){
+			expect(projectLayout.innerMarginForm.isDisplayed()).toBe(true);
+		});
+		it('should have text: Top Margin', function(){
+			expect(projectLayout.innerMarginForm.isDisplayed()).toBe(true);
+		});
+		it('should have text: Bottom Margin', function(){
+			expect(projectLayout.innerMarginForm.isDisplayed()).toBe(true);
+		});
+		it('should set the values', function(){	
+			projectLayout.innerMarginForm.clear().then(function(){
+				projectLayout.innerMarginForm.sendKeys('80');
+			});
+			projectLayout.outerMarginForm.clear().then(function(){
+				projectLayout.outerMarginForm.sendKeys('30');
+			});
+			projectLayout.topMarginForm.clear().then(function(){
+				projectLayout.topMarginForm.sendKeys('60');
+			});
+			projectLayout.bottomMarginForm.clear().then(function(){
+				projectLayout.bottomMarginForm.sendKeys('150');
+			});
+		});
+	});
+		
+		
+	//	.sendKeys('myemail@myemail.com');
+});
