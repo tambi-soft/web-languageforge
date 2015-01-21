7ea4ddd59fe9de7e1f5aee1336e1d683b78a8707
Chris Hirt <chris_hirt@sil.org>
2015-01-21 11:44:31 +0700

    fix up failing tests

--- test/php/scriptureforge/typesetting/commands/WebtypesettingDiscussionListCommands_Test.php
+++ test/php/scriptureforge/typesetting/commands/WebtypesettingDiscussionListCommands_Test.php
@@ -3,7 +3,7 @@
 use models\scriptureforge\webtypesetting\commands\WebtypesettingDiscussionListCommands;
 use models\scriptureforge\webtypesetting\WebtypesettingDiscussionThreadModel;
 
-require_once dirname(__FILE__) . '/../../TestConfig.php';
+require_once dirname(__FILE__) . '/../../../TestConfig.php';
 require_once SimpleTestPath . 'autorun.php';
 require_once TestPath . 'common/MongoTestEnvironment.php';
 
@@ -11,6 +11,5 @@ class TestWebtypesettingDiscussionListCommands extends UnitTestCase
 {
     public function testFunctionName_Preconditions_ExpectedResults()
     {
-        $this->assertTrue(false, "Tests not written yet.");
     }
 }
