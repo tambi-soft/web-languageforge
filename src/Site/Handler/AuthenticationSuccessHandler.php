3325c03a95b3443cebfe2e874a2e05f48dfb3e14
irahopkinson <ira_hopkinson@sil.org>
2015-10-01 16:31:32 +0700

    feature/silex: fix routing at login + minor tidy-ups

--- src/Site/Handler/AuthenticationSuccessHandler.php
+++ src/Site/Handler/AuthenticationSuccessHandler.php
@@ -8,60 +8,53 @@ use Api\Model\Shared\Rights\SiteRoles;
 use Api\Model\Shared\Rights\SystemRoles;
 use Api\Model\UserModel;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessHandler;
 use Symfony\Component\Security\Http\HttpUtils;
 
 class AuthenticationSuccessHandler extends DefaultAuthenticationSuccessHandler
 {
-    public function __construct(HttpUtils $httpUtils, array $options, Session $session) {
-        parent::__construct($httpUtils, $options);
-        $this->session = $session;
-    }
-
     /**
-     * @var Session
+     * Constructor.
+     *
+     * @param HttpUtils $httpUtils
+     * @param array     $options   Options for processing a successful authentication attempt.
+     * @param string|null $providerKey
      */
-    protected $session = null;
+    public function __construct(HttpUtils $httpUtils, array $options = array(), $providerKey = null) {
+        parent::__construct($httpUtils, $options);
+        $this->setProviderKey($providerKey);
+    }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token) {
         $username = $token->getUser()->getUsername();
         $user = new UserModel();
-        if (! $user->readByUserName($username)) {
+        $website = Website::get();
+        if (! $user->readByUserName($username) or
+            (($user->role != SystemRoles::SYSTEM_ADMIN) and
+            !($user->siteRole->offsetExists($website->domain) and
+                ($user->siteRole[$website->domain] != SiteRoles::NONE)))) {
             return $this->httpUtils->createRedirectResponse($request, '/app/logout');
         }
 
-        $this->session->set('user_id', $user->id->asString());
-        $this->session->set('user', array('username' => $username));
+        $request->getSession()->set('user_id', $user->id->asString());
+        $request->getSession()->set('user', array('username' => $username));
 
-        // Validate user is admin or has a role on the site.  Otherwise, redirect to logout
-        $website = Website::get();
-        if (($user->role == SystemRoles::SYSTEM_ADMIN) ||
-            ($user->siteRole->offsetExists($website->domain) &&
-                ($user->siteRole[$website->domain] != SiteRoles::NONE))) {
-            // set the project context to user's default project
-            $projectId = $user->getDefaultProjectId($website->domain);
+        $projectId = $user->getDefaultProjectId($website->domain);
+        if ($projectId) {
+            $request->getSession()->set('projectId', $projectId);
+        }
 
-            if ($projectId) {
-                $this->session->set('projectId', $projectId);
-            }
+        $referer = $this->determineTargetUrl($request);
+        if ($referer and strpos($referer, '/app/') !== false) {
+            return $this->httpUtils->createRedirectResponse($request, $referer);
+        } elseif ($projectId) {
+            $project = ProjectModel::getById($projectId);
+            $url = '/app/'.$project->appName.'/'.$projectId;
 
-            $referer = $this->determineTargetUrl($request);
-            if ($referer && strpos($referer, '/app') !== false) {
-                return $this->httpUtils->createRedirectResponse($request, $referer);
-            } elseif ($projectId) {
-                $project = ProjectModel::getById($projectId);
-                $url = '/app/'.$project->appName.'/'.$projectId;
-
-                return $this->httpUtils->createRedirectResponse($request, $url);
-            } else {
-                return $this->httpUtils->createRedirectResponse($request, '/');
-            }
+            return $this->httpUtils->createRedirectResponse($request, $url);
         } else {
-            $this->session->getFlashBag()->add('message', 'You are not authorized to use this site');
-
-            return $this->httpUtils->createRedirectResponse($request, '/app/logout');
+            return $this->httpUtils->createRedirectResponse($request, '/');
         }
     }
 }
